9b0498e5193b837e3b45da120a2e5a71396fc2a5
Yash Kumar Gupta <yash@Yashs-MacBook-Air.local>
2026-04-26 13:58:51 +0530

    feat(lexer): Implement basic lexer for Monk language

---
+++ lexer/lexer.go
@@ -0,0 +1,112 @@
+package lexer
+
+import "github.com/AuraReaper/monk/token"
+
+type Lexer struct {
+	input         string
+	postition     int  // current pos in input
+	readPostition int  // current read pos in input
+	ch            byte // curr char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// returns next char and advance pos in input
+func (l *Lexer) readChar() {
+	if l.readPostition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPostition]
+	}
+
+	l.postition = l.readPostition
+	l.readPostition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhiteSpace()
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{
+		Type:    tokenType,
+		Literal: string(ch),
+	}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.postition
+
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.postition]
+}
+
+func isLetter(ch byte) bool {
+	return ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) skipWhiteSpace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func isDigit(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.postition
+
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.postition]
+}
